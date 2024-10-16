86d5954c94c251e00f0572b4202af394b816deae
Ismail <ismail.hamdi@zeply.com>
2024-10-16 01:56:38 +0100

    feat: left menu section

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,29 @@
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      dashboard layout
-      {children}
+    <div className="h-screen flex">
+      {/* left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[16%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.jpeg" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block text-gray-600 font-bold">
+            Polytech INTL
+          </span>
+        </Link>
+        <Menu />
+      </div>
+      {/* right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[16%]">{children}</div>
     </div>
   );
 }
