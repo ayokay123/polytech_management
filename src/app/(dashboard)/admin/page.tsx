ec23b930088d77487862665e2842dcee6c77b08a
Ismail <ismail.hamdi@zeply.com>
2024-10-16 11:39:45 +0100

    feat: admin-user cards

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,22 @@
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row ">
+      {/* Left */}
+      <div className="w-full lg:w-2/3">
+        {/* User Cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="students" />
+          <UserCard type="teachers" />
+          <UserCard type="parents" />
+          <UserCard type="staffs" />
+        </div>
+      </div>
+      {/* RIght */}
+      <div className="w-full lg:w-1/3">right</div>
+    </div>
+  );
 };
 
 export default AdminPage;
