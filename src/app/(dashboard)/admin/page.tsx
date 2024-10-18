6ff3c107e75eb7f68078341d390f57437eb3f9b2
Ismail <ismail.hamdi@zeply.com>
2024-10-18 01:00:58 +0100

    feat: admin-attendance chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
@@ -19,7 +20,7 @@ const AdminPage = () => {
             <CountChart />
           </div>
           <div className="w-full lg:w-2/3 h-[450px]">
-            {/* <CountChart /> */}
+            <AttendanceChart />
           </div>
         </div>
       </div>
